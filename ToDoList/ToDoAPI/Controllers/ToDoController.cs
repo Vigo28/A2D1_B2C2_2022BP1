ec84019f9ce28296ee9a2825370f0be766cd86a3
Vigo <2101466luthjens@zuyd.nl>
2023-10-04 10:38:04 +0200

    Done

--- ToDoList/ToDoAPI/Controllers/ToDoController.cs
+++ ToDoList/ToDoAPI/Controllers/ToDoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoListModel.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,34 +11,40 @@ namespace ToDoAPI.Controllers
     {
         // GET: api/<ToDoController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<ToDoTask> Get()
         {
-            return new string[] { "value1", "value2" };
+            return ToDoTask.ReadAll();
         }
 
         // GET api/<ToDoController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ToDoTask Get(int id)
         {
-            return "value";
+            return ToDoTask.Read(id);
         }
 
         // POST api/<ToDoController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] string description)
         {
+            ToDoTask newTask = new ToDoTask(description);
+            newTask.Create();
         }
 
         // PUT api/<ToDoController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("{idToFinish}")]
+        public void Put(int idToFinish)
         {
+            ToDoTask finishTask = ToDoTask.Read(idToFinish);
+            finishTask.FinishTask();
         }
 
         // DELETE api/<ToDoController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            ToDoTask deleteTask = ToDoTask.Read(id);
+            deleteTask.Delete();
         }
     }
 }
